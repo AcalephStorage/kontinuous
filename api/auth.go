d261a16d4f12d65633049dd37875c0c0f2b12250
dexter <dgenterone@acale.ph>
2016-05-04 17:28:37 +0800

    fix for github issue. needs cleanup though

--- api/auth.go
+++ api/auth.go
@@ -6,11 +6,18 @@ import (
 	"strings"
 
 	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/emicklei/go-restful"
 )
+
+type GithubAuthResponse struct {
+	AccessToken string `json:"access_token"`
+}
 
 // JWTClaims contains the claims from the jwt
 type JWTClaims struct {
@@ -81,11 +88,56 @@ func (a *AuthResource) Register(container *restful.Container) {
 
 func (a *AuthResource) authorize(req *restful.Request, res *restful.Response) {
 	dsecret, _ := base64.URLEncoding.DecodeString(os.Getenv("AUTH_SECRET"))
-	accessToken := req.QueryParameter("access_token")
-	if len(accessToken) == 0 {
-		jsonError(res, http.StatusBadRequest, errors.New("Missing Access Token!"), "Unable to find access token")
+	authCode := req.QueryParameter("code")
+	state := req.QueryParameter("state")
+
+	if len(authCode) == 0 {
+		jsonError(res, http.StatusUnauthorized, errors.New("Missing Authorization Code"), "No authorization code provided")
 		return
 	}
+
+	// request url
+	reqUrl := url.URL{
+		Scheme: "https",
+		Host:   "github.com",
+		Path:   "login/oauth/access_token",
+	}
+	q := reqUrl.Query()
+	q.Set("client_id", os.Getenv("GH_CLIENT_ID"))
+	q.Set("client_secret", os.Getenv("GH_CLIENT_SECRET"))
+	q.Set("code", authCode)
+	q.Set("state", state)
+	reqUrl.RawQuery = q.Encode()
+
+	client := &http.Client{}
+
+	r, err := http.NewRequest("POST", reqUrl.RequestURI(), nil)
+	if err != nil {
+		jsonError(res, http.StatusUnauthorized, err, "Error creating auth request")
+		return
+	}
+	r.Header.Add("Accepts", "application/json")
+
+	authRes, err := client.Do(r)
+	if err != nil {
+		jsonError(res, http.StatusUnauthorized, err, "Error requesting authorization token")
+		return
+	}
+	defer authRes.Body.Close()
+
+	body, err := ioutil.ReadAll(authRes.Body)
+	if err != nil {
+		jsonError(res, http.StatusUnauthorized, err, "Error reading response body")
+		return
+	}
+
+	var ghRes GithubAuthResponse
+	if err := json.Unmarshal(body, &ghRes); err != nil {
+		jsonError(res, http.StatusUnauthorized, err, "Error reading json body")
+		return
+	}
+
+	accessToken := ghRes.AccessToken
 
 	jwtToken, err := CreateJWT(accessToken, string(dsecret))
 
